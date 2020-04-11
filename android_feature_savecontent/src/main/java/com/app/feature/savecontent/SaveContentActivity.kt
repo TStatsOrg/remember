76602ee021a5866fe4f24eee58ca264a6b5644ce
Gabriel Coman <dev.gabriel.coman@gmail.com>
2020-04-11 14:37:57 +0100

    fix(feature): first iteration of save content feature

---
+++ android_feature_savecontent/src/main/java/com/app/feature/savecontent/SaveContentActivity.kt
@@ -0,0 +1,11 @@
+package com.app.feature.savecontent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SaveContentActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
