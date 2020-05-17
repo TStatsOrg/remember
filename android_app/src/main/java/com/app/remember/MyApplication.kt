28a33620f15843dcb823a155c9048bbce56d6abf
Gabriel Coman <dev.gabriel.coman@gmail.com>
2020-05-17 14:55:04 +0100

    better way to subscribe and unsubscribe

--- android_app/src/main/java/com/app/remember/MyApplication.kt
+++ android_app/src/main/java/com/app/remember/MyApplication.kt
@@ -1,10 +1,7 @@
 package com.app.remember
 
 import android.app.Application
-//import com.app.shared.observ.InfiniteEmitter2
-//import com.app.shared.observ.ObservableEmitter2
-//import com.app.shared.observ.SimpleObserver2
-import com.app.shared.observ.map
+import com.app.shared.observ.*
 import com.app.shared.utils.MLogger
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -23,33 +20,31 @@ class MyApplication: Application() {
             modules(provider.module)
         }
 
-//        val emitter = ObservableEmitter2<Int>()
-//        val observer1 = emitter.addObserver()
-//            .map {
-//                "String value is $it"
-//            }
-//            .collect {
-//                MLogger.log("GABBOX => Observer 1 => Collected: $it")
-//            }
-//        val observer2 = emitter.addObserver()
-//            .map {
-//                it * 2
-//            }
-//            .collect {
-//                MLogger.log("GABBOX => Observer 2 => Collected: $it")
-//            }
-//
-//        MLogger.log("GABBOX Total observers => ${emitter.observers.size}")
-//
-//        emitter.emit(2)
-//        emitter.emit(3)
-//        emitter.detach(observer = observer2)
-//        emitter.emit(5)
-//
-//        MLogger.log("GABBOX Total observers => ${emitter.observers.size}")
-//
-//        MLogger.log("GABBOX $observer1")
-//        MLogger.log("GABBOX $observer2")
-//        MLogger.log("GABBOX ${emitter.observers}")
+        val emitter = InfiniteEmitter2<Int>()
+
+        val observer1 = SimpleObserver2<Int> { value ->
+            MLogger.log("GABBOX: Observer 1 => $value")
+        }
+
+        val observer2 = SubscribingObserver2<Int>()
+        observer2.subscriber.map {
+            it * 2
+        }.collect { value ->
+            MLogger.log("GABBOX: Observer 2 => $value")
+        }
+
+        emitter.add(observer = observer1)
+        emitter.add(observer = observer2)
+
+        MLogger.log("GABBOX: Number of remaining observers ${emitter.observers.size}")
+
+        emitter.emit(value = 1)
+        emitter.emit(value = 2)
+        emitter.emit(value = 5)
+
+        emitter.remove(observer1)
+        emitter.remove(observer2)
+
+        MLogger.log("GABBOX: Number of remaining observers ${emitter.observers.size}")
     }
 }
--- shared/src/commonMain/kotlin/com/app/shared/observ/Subject.kt
+++ shared/src/commonMain/kotlin/com/app/shared/observ/Subject.kt
@@ -1,20 +1,82 @@
 package com.app.shared.observ
 
-interface Subject<T> {
-    fun emit(value: T)
-    fun collect(callback: (T) -> Unit): Subject<T>
+interface Subscriber<T> {
+    fun collect(callback: (T) -> Unit)
 }
 
-class InfiniteSubject<T>: Subject<T> {
+class SimpleSubscriber<T>: Subscriber<T> {
 
-    private val observers = mutableListOf<(T) -> Unit>()
+    internal var function: ((T) -> Unit)? = null
 
-    override fun emit(value: T) {
-        observers.forEach { it.invoke(value) }
+    override fun collect(callback: (T) -> Unit) {
+        function = callback
+    }
+}
+
+fun <T, U> Subscriber<T>.map(transform: (T) -> U): Subscriber<U> {
+    return object : Subscriber<U> {
+        override fun collect(callback: (U) -> Unit) {
+            this@map.collect {
+                callback(transform(it))
+            }
+        }
+    }
+}
+
+fun <T> Subscriber<T?>.filterNotNull(): Subscriber<T> {
+    return object : Subscriber<T> {
+        override fun collect(callback: (T) -> Unit) {
+            this@filterNotNull.collect {
+                it?.let { callback(it) }
+            }
+        }
+    }
+}
+
+interface Observer2<T> {
+    fun didChange(value: T)
+}
+
+class SimpleObserver2<T>(private val func: ((T) -> Unit)? = null): Observer2<T> {
+
+    override fun didChange(value: T) {
+        func?.invoke(value)
+    }
+}
+
+class SubscribingObserver2<T>: Observer2<T> {
+
+    var subscriber: SimpleSubscriber<T> = SimpleSubscriber()
+
+    override fun didChange(value: T) {
+        subscriber.function?.invoke(value)
+    }
+}
+
+//fun <T, U> SubscribingObserver2<T>.map(transform: (T) -> U): SubscribingObserver2<U> {
+//    val newSub = SubscribingObserver2<U>()
+//    newSub.subscriber = this.subscriber.map(transform)
+//}
+
+interface Emitter2<T> {
+    fun add(observer: Observer2<T>)
+    fun remove(observer: Observer2<T>)
+    fun emit(value: T)
+}
+
+class InfiniteEmitter2<T>: Emitter2<T> {
+
+    val observers = mutableListOf<Observer2<T>>()
+
+    override fun add(observer: Observer2<T>) {
+        observers.add(observer)
     }
 
-    override fun collect(callback: (T) -> Unit): Subject<T> {
-        observers.add(callback)
-        return this
+    override fun remove(observer: Observer2<T>) {
+        observers.remove(observer)
+    }
+
+    override fun emit(value: T) {
+        observers.forEach { it.didChange(value = value) }
     }
 }
