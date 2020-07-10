b667204286c82a7ce3711abee35975dc98e81c88
Gabriel Coman <dev.gabriel.coman@gmail.com>
2020-07-10 13:39:29 +0100

    fix(tablet): added tablet support for the "Home" or "User Feed" page

--- ios_app/ios_feature_feed_user/ios_feature_feed_user/UserFeedsView.swift
+++ ios_app/ios_feature_feed_user/ios_feature_feed_user/UserFeedsView.swift
@@ -11,20 +11,32 @@ import ios_views
 import ios_dependencies
 import RememberShared
 import SDWebImageSwiftUI
+import Grid
 
 public struct UserFeedsView: View {
     
     @Injected private var navigation: Navigation
     @Injected private var viewModel: FeedsViewModel
+    @Injected private var deviceUtils: DeviceUtils
     @State private var isShowingSheet: Bool = false
     @State private var state: UserFeedsViewState = UserFeedsViewState()
     
+    private var noGridTracks: Tracks {
+        return deviceUtils.isTablet() ? 2 : 1
+    }
+
     public init() { /* n/a */ }
     
     public var body: some View {
         VStack {
             ManagedEmptyView(state: state.empty)
-            List(state.items, rowContent: buildRow)
+            ScrollView {
+                Grid(state.items, item: buildRow)
+                .gridStyle(
+                    StaggeredGridStyle(.vertical, tracks: noGridTracks, spacing: 8)
+                )
+                .padding(EdgeInsets(top: 12, leading: 12, bottom: 0, trailing: 12))
+            }
         }
         .navigationBarTitle(Text(Translations.UserFeeds.title), displayMode: .inline)
         .navigationBarItems(trailing: Button(action: {
